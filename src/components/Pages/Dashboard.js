1d1d70c97b05c0979d98532687dd6ece23bff97b
Pedrocardo8 <pedrocardo8@gmail.com>
2021-03-17 11:36:18 +0000

    Alterações dashboard

--- src/components/Pages/Dashboard.js
+++ src/components/Pages/Dashboard.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Card, Button, Container, Row,Col } from 'react-bootstrap';
+import './Dashboard.css';
 
 function Dashboard() {
     return (
         <div>
-            <h1>Dashboard</h1>
+            <Container>
+                <Row className="mt-3">
+                    <Col className="total">
+                        <h3>Total</h3>
+                        <p>2000€</p>
+                    </Col>
+
+                    <Col>
+                        <h3>Expenses</h3>
+                        <p>2000€</p>
+                    </Col> 
+
+                    <Col>
+                        <h3>Earned Money</h3>
+                        <p>2000€</p>
+                    </Col>
+
+                    <Col>
+                        <h3>Budget</h3>
+                        <p>2000€</p>
+                    </Col>
+            </Row>
+            </Container>
         </div>
     )
 }
